7efda8f5b8c494eaf490849917186b6a63acccb5
Griffin <garam.dev9@gmail.com>
2021-02-17 19:32:06 +0900

    Create: 모델 js
    - 사용자, 게시물, 해시태그

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+
+module.exports = class  User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: true,
+                unique: true,
+            },
+            nick: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+                defaultValue: 'local',  // local: 로컬로그인, kakao: 카카오로그인
+            },
+            snsId: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            },
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+    
+    static associate(db) {}
+};
