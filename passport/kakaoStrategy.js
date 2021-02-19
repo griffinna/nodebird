1ebd53a6b916e380e4d5c86f135f0586017492c7
Griffin <garam.dev9@gmail.com>
2021-02-19 21:31:13 +0900

    Update: 카카오 로그인 설명

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -5,10 +5,10 @@ const User = require('../models/user');
 
 module.exports = () => {
     passport.use(new KakaoStrategy({
-        clientID: process.env.KAKAO_ID,
-        callbackURL: '/auth/kakao/callback',
+        clientID: process.env.KAKAO_ID,         // 카카오로부터 발급받은 ID (.env 에 저장)
+        callbackURL: '/auth/kakao/callback',    // 카카오로부터 인증결과를 받을 라우터 주소
     }, async (accessToken, refreshToken, profile, done) => {
-        console.log('kakao profile', profile);
+        console.log('kakao profile', profile);  // profile: 카카오에서 보내주는 사용자 정보
         try {
             const exUser = await User.findOne({
                 where: { snsId: profile.id, provider: 'kakao'},
