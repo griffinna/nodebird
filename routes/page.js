a140b92dab27c5ada77425c6959f1e8cc6e45b7c
Griffin <garam.dev9@gmail.com>
2021-02-22 21:22:08 +0900

    Add: 해시태그 조회 라우터

--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const { Post, User } = require('../models');
+const { Post, User, Hashtag } = require('../models');
 
 const router = express.Router();
 
@@ -46,6 +46,28 @@ router.get('/', async (req, res, next) => {
     } catch (err) {
         console.error(err);
         next(err);
+    }
+})
+
+router.get('/hashtag', async (req, res, next) => {
+    const query = req.query.hashtag;
+    if(!query) {
+        return res.redirect('/');
+    }
+    try {
+        const hashtag = await Hashtag.findOne({ where: { title: query }});
+        let posts = [];
+        if(hashtag) {
+            posts = await hashtag.getPosts({ include: [{ model: User }] });
+        }
+
+        return res.render('main', {
+            title: `${query} | NodeBird`,
+            twits: posts,
+        });
+    } catch (error) {
+        console.error(error);
+        next(error);
     }
 })
 
