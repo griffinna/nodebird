869274431b72da3026a934d8c914fb3b2b627e3a
Griffin <garam.dev9@gmail.com>
2021-02-22 21:18:26 +0900

    Update: 팔로잉/팔로워 숫자와 팔로우 버튼 표시

--- routes/page.js
+++ routes/page.js
@@ -7,9 +7,9 @@ const router = express.Router();
 router.use((req, res, next) => {
     // 템플릿 엔진에서 공통으로 사용하는 값을 res.locals 에 넣음
     res.locals.user = req.user; // 넌적스에서 user 객체를 통해 사용자 정보에 접근 가능
-    res.locals.followerCount = 0;
-    res.locals.followingCount = 0;
-    res.locals.followerIdList = [];
+    res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+    res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+    res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
     next();
 });
 
