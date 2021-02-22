08502262b739bbe7bd2460f6842137b1d1c50cb2
Griffin <garam.dev9@gmail.com>
2021-02-22 21:16:46 +0900

    Add: 사용자 정보 조회시 팔로잉/팔로워 목록 조회

--- passport/index.js
+++ passport/index.js
@@ -14,7 +14,18 @@ module.exports = () => {
     // .deserializeUser : 매 요청 시 실행 (passport.session 미들웨어가 이 메서드를 호출)
     // 세션에 저장한 아이디를 통해 사용자정보 객체를 불러오는 것
     passport.deserializeUser((id, done) => {    // done의 두번째 인수로 넣은 데이터가 매개변수가 됨
-        User.findOne({ where: {id} })
+        User.findOne({  // 사용자 정보 조회할 때 팔로잉, 팔로워 목록도 함께 조회
+            where: {id},
+            include: [{
+                model: User,
+                attributes: ['id', 'nick'],
+                as: 'Followes',
+            }, {
+                model: User,
+                attributes: ['id', 'nick'],
+                as: 'Followings'
+            }],
+        })
             .then(user => done(null, user)) // req.user 에 저장
             .catch(err => done(err));
     });
