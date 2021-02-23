f6bf3c0a726892b98e0be55e614941b5b99d2233
Griffin <garam.dev9@gmail.com>
2021-02-23 22:12:24 +0900

    Add: 팔로우 취소 delete 라우터

--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,39 @@
 const express = require('express');
 
 const { isLoggedIn } = require('./middlewares');
+const { sequelize } = require('../models');
 const User = require('../models/user');
 
 const router = express.Router();
 
-router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
-    try {
-        const user = await User.findOne({ where: { id: req.user.id } });
-        if(user) {
-            await user.addFollowing(parseInt(req.params.id, 10));
-            res.send('success');
-        } else {
-            res.status(404).send('No User');
+router.route('/:id/follow')
+    .post(isLoggedIn, async (req, res, next) => {
+        try {
+            const user = await User.findOne({ where: { id: req.user.id } });
+            if(user) {
+                await user.addFollowing(parseInt(req.params.id, 10));
+                res.send('success');
+            } else {
+                res.status(404).send('No User');
+            }
+        } catch (error) {
+            console.error(error);
+            next(error);
         }
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-});
+    })
+    .delete(isLoggedIn, async (req, res, next) => {
+        try {
+            const user = await User.findOne({ where: { id: req.user.id } });
+            if(user) {
+                const [result, metadata] = await sequelize.query('DELETE FROM Follow WHERE followingId = ' + req.params.id + ' AND followerId = ' + req.user.id);
+                res.send('success');
+            } else {
+                res.status(404).send('No User');
+            }
+        } catch (error) {
+            console.error(error);
+            next(error);
+        }
+    })
 
 module.exports = router;
