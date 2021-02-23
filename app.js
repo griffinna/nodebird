caeff9210a67184548768a04055389bce53cf030
Griffin <garam.dev9@gmail.com>
2021-02-23 19:33:31 +0900

    Add: Post, User 라우터 app 연결

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ const dotenv = require('dotenv');
 const passport = require('passport');
 
 dotenv.config();
+
+// 라우터 선언
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
+
 // DB 모델 연결
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
@@ -34,6 +39,7 @@ sequelize.sync({ force: false })
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));   // 이미지 업로드시 사용할 경로
 app.use(express.json());
 app.use(express.urlencoded({ extends: false }));
 app.use(cookieParseer(process.env.COOKIE_SECRET));
@@ -51,8 +57,11 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// 라우터 연결
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
