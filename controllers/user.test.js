78e45c0f02f04db39ba2518d9bea8a6f1b1f8b7a
Griffin <garam.dev9@gmail.com>
2021-03-08 21:52:11 +0900

    Add: User mock

--- controllers/user.test.js
+++ controllers/user.test.js
@@ -1,4 +1,5 @@
-const { TestScheduler } = require('@jest/core');
+jest.mock('../models/user');    // 모킹할 모듈 지정
+const User = require('../models/user');
 const { addFollowing } = require('./user');
 
 describe('addFollowing', () => {
@@ -14,11 +15,18 @@ describe('addFollowing', () => {
     const next = jest.fn();
 
     test('사용자를 찾아 팔로잉을 추가하고 success 를 응답해야 함', async () => {
+        // mockReturnValue: 가짜 반환값을 지정
+        User.findOne.mockReturnValue(Promise.resolve({
+            addFollowing(id) {
+                return Promise.resolve(true);
+            }
+        }));
         await addFollowing(req, res, next);
         expect(res.send).toBeCalledWith('success');
     });
 
     test('사용자를 못 찾으면 res.status(404).send(no user)를 호출함', async () => {
+        User.findOne.mockReturnValue(null);
         await addFollowing(req, res, next);
         expect(res.status).toBeCalledWith(404);
         expect(res.send).toBeCalledWith('no user');
@@ -26,6 +34,7 @@ describe('addFollowing', () => {
 
     test('DB 에서 에러가 발생하면 next(error) 호출함', async () => {
         const error = '테스트용 에러';
+        User.findOne.mockReturnValue(Promise.reject(error));    // 강제로 에러 발생
         await addFollowing(req, res, next);
         expect(next).toBeCalledWith(error);
     });
