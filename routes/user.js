c9e816d8a030a314c87c64541911a2c5813f2e3e
Griffin <garam.dev9@gmail.com>
2021-03-08 21:43:49 +0900

    Modify: user follow 라우터 controller 분리

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,14 @@
 const express = require('express');
 
 const { isLoggedIn } = require('./middlewares');
+const { addFollowing } = require('../controllers/user');
 const { sequelize } = require('../models');
 const User = require('../models/user');
 
 const router = express.Router();
 
 router.route('/:id/follow')
-    .post(isLoggedIn, async (req, res, next) => {
-        try {
-            const user = await User.findOne({ where: { id: req.user.id } });
-            if(user) {
-                await user.addFollowing(parseInt(req.params.id, 10));
-                res.send('success');
-            } else {
-                res.status(404).send('No User');
-            }
-        } catch (error) {
-            console.error(error);
-            next(error);
-        }
-    })
+    .post(isLoggedIn, addFollowing)
     .delete(isLoggedIn, async (req, res, next) => {
         try {
             const user = await User.findOne({ where: { id: req.user.id } });
